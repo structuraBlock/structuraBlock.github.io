057c921423cb40c2859aa11d2d2f6cef9daeff16
structuraJS <168809916+structuraJS@users.noreply.github.com>
2024-05-10 12:48:55 +0800

    list.js

--- list.js
+++ list.js
@@ -1,21 +1,68 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-function walkDir(dir) {
+const pathList = new Set()
+
+// clear
+fs.writeFileSync('./list.txt','', { flag: 'w' })
+
+
+
+const walkDir = (dir)=> fs.readdirSync(dir).forEach(f => {
+        const fullPath = path.join(dir, f)
+        const relativePath = path.relative(process.cwd(), fullPath).replaceAll('\\','/')
+        
+            // 如果是目录，则跑路
+        if (!fs.statSync(fullPath).isDirectory()) 
+            pathList.add(relativePath)
+            // console.log(relativePath)
+
+})
+
+
+const walkDirPro = (dir)=> {
     fs.readdirSync(dir).forEach(f => {
-        const fullPath = path.join(dir, f);
-        const relativePath = path.relative(process.cwd(), fullPath);
+        const fullPath = path.join(dir, f)
+        const relativePath = path.relative(process.cwd(), fullPath).replaceAll('\\','/')
         
         if (fs.statSync(fullPath).isDirectory()) {
             // 如果是目录，则递归调用自身
-            walkDir(fullPath);
+            walkDirPro(fullPath)
         } else {
             // 如果是文件，则按要求格式打印路径
-            fs.writeFileSync('./list.txt', `"${relativePath.replace(/\\/g, '/')}" = "./${relativePath}"\n`, { flag: 'a' });
-            console.log(`"${relativePath.replace(/\\/g, '/')}" = "./${relativePath}"`);
+            pathList.add(relativePath)
+            
+            // console.log(relativePath)
         }
-    });
+    })
 }
 
+// no no includes file
+const igoreFile = [
+    ".gitattributes",
+    ".gitignore",
+    "list.txt",
+    "list.js",
+    "package-lock.json",
+    "package.json",
+    "pyscript.toml",
+    "README.md",
+    "requirements.txt",
+    "updater.py",
+    "index.js",
+    "index.html",
+    "upTest.html",
+    "speedTest.html",
+    "APItest.html",
+]
 // 从当前目录开始遍历
-walkDir('./');
+walkDir('./')
+igoreFile.forEach(_=>pathList.delete(_))
+walkDirPro('./lookups')
+walkDirPro('./test_structures')
+walkDirPro('./Vanilla_Resource_Pack')
+
+pathList.forEach(relativePath=>
+    fs.writeFileSync('./list.txt', `"${relativePath}" = "./${relativePath}"\n`, { flag: 'a' })
+)
+console.log(pathList.size)
