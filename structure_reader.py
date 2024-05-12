a4ddf3718d4b44c79548883181e4711aafada4ae
structuraJS <168809916+structuraJS@users.noreply.github.com>
2024-05-12 10:57:19 +0800

    numpy.whl is too big

--- structure_reader.py
+++ structure_reader.py
@@ -1,4 +1,5 @@
 import nbtlib
+# numpy-1.26.4-cp311-cp311-emscripten_3_1_46_wasm32.whl -- > 12MB ???? what?
 from numpy import array, argwhere , int32, maximum, minimum, zeros, count_nonzero, flip
 import json
 loaded={}
