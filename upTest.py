cf98c38eefe5eb338faad611fac4501a9706b1e8
structuraJS <168809916+structuraJS@users.noreply.github.com>
2024-05-22 01:02:00 +0800

    gui test

--- upTest.py
+++ upTest.py
@@ -4,16 +4,17 @@ import structura_core
 import shutil
 structura_core.debug=True
 
-def conver(name_tag,file,offset=[-32,0,-31],opacity=20):
+try:
+    shutil.rmtree("tmp/")
+except:
+    pass
+structura_base = structura_core.structura("tmp/love")
+structura_base.set_opacity(20)
+
+def add(name_tag,file,offset=[-32,0,-31],opacity=20):
     print(">> so do i")
     nbt = nbtlib.File.from_fileobj(file, byteorder='little')
 
-    try:
-        shutil.rmtree("tmp/")
-    except:
-        pass
-    structura_base = structura_core.structura("tmp/"+name_tag)
-    structura_base.set_opacity(opacity)
 
 
     print(f'>> 名称标签-->{name_tag}, 坐标偏移--> {offset}')
@@ -21,6 +22,10 @@ def conver(name_tag,file,offset=[-32,0,-31],opacity=20):
     structura_base.add_model(name_tag,nbt)
     structura_base.set_model_offset(name_tag,offset)
 
+
+
+
+def conver():
     # structura_base.generate_nametag_file()
     structura_base.generate_with_nametags()
     info = structura_base.compile_pack()
@@ -29,5 +34,3 @@ def conver(name_tag,file,offset=[-32,0,-31],opacity=20):
 
     print(">> Done!!")
     print(">> 完成啦！！")
-
-
